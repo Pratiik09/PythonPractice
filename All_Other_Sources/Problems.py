ac27f757da13486975bbefea457f409ec028505c
Pratiik09 <pratikpadman01@gmail.com>
2023-02-06 13:44:18 +0530

    Added New Problems

--- All_Other_Sources/Problems.py
+++ All_Other_Sources/Problems.py
@@ -26,3 +26,53 @@ print(transpose)
 
 # OUTPUT:
 [[1, 4], [2, 5], [3, 6]]
+
+### Reverse Integer without converting to String ###
+
+n = 1234
+r = 0
+
+while n > 0:
+    r = r*10 + n%10
+    n = n//10
+print(r)
+
+
+### Print Fibonacci Series using Iterative Approach ###
+a, b = 0, 1
+
+n = 7  # Number of Fibonacci Elements
+
+fib_list = []
+
+for i in range(0, n):
+    if i <= 1:
+        c = i
+    else:
+        c = a + b
+        a = b
+        b = c
+    fib_list.append(c)
+    
+print(fib_list)
+
+### Print Fibonacci Series using Recursive Approach ###
+num = 7  # Number of Fibonacci Elements
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-2) + fibonacci(n-1)
+       
+fib_list = []
+
+for i in range(0,num):
+    fib_list.append( fibonacci(i) )
+
+print(fib_list)
+
+
+###  ###
